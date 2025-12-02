cef927802a9aa08e830305b7121927c8ad2eba74
Dora Liao <doraliao@crc-dot1x-nat-10-239-74-18.bu.edu>
2025-12-02 01:32:43 -0500

    corr

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -3,4 +3,7 @@ import { createClient } from '@supabase/supabase-js';
 const SUPABASE_URL =  process.env.REACT_APP_SUPABASE_URL;
 const SUPABASE_ANON_KEY = process.env.REACT_APP_SUPABASE_KEY;
 
+console.log("URL:", process.env.REACT_APP_SUPABASE_URL);
+console.log("KEY:", process.env.REACT_APP_SUPABASE_KEY ? "Loaded" : "Missing");
+
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
