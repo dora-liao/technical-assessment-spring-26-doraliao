a21c371c6a9e6f3249842f5a50bc79d61278c16b
Dora Liao <doraliao@crc-dot1x-nat-10-239-74-18.bu.edu>
2025-12-02 01:02:09 -0500

    secrets hidden

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const SUPABASE_URL = 'https://rxcroiufriodknmnyugt.supabase.co';
-const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJ4Y3JvaXVmcmlvZGtubW55dWd0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjM5MzE2MDksImV4cCI6MjA3OTUwNzYwOX0.gn0Cp--f-9nQ138jD_sbIH9bzhk_UzjK0D4OZqtyNLg';
+const SUPABASE_URL = process.env.SUPABASE_URL; 
+const SUPABASE_ANON_KEY = process.env.REACT_APP_SUPABASE_KEY;
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
